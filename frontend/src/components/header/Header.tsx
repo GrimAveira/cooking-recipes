a550f953461a583a8ff398b7834193bfa2469444
grimaveira <st.tryed@gmail.com>
2023-12-05 12:25:47 +0400

    Logo and blur in Header components

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,9 +1,13 @@
 import Button from "../button/Button";
+import styles from "./Header.module.scss";
+import { logo } from "../../assets";
 
 function Header() {
   return (
-    <header>
-      <Button title="Вход" />
+    <header className={styles.container}>
+      <div className={styles.blur}></div>
+      <img className={styles.logo} src={logo} />
+      <Button title="Вход" className={styles.button} />
     </header>
   );
 }
