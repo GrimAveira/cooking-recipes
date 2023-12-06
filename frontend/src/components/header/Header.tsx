5fdfa4f1c69d69d2220c9c8db9a0273f2efcfa55
GrimAveira <st.tryed@gmail.com>
2023-12-06 21:38:35 +0400

    Added burger menu icon Header.tsx

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,15 +1,16 @@
 import Button from "../button/Button";
 import styles from "./Header.module.scss";
-import { logo } from "../../assets";
+import { logo, menu } from "../../assets";
 
-function Header() {
+const Header = () => {
   return (
     <header className={styles.container}>
+      <img className={styles.menu} src={menu} />
       <div className={styles.blur}></div>
       <img className={styles.logo} src={logo} />
       <Button title="Вход" className={styles.button} />
     </header>
   );
-}
+};
 
 export default Header;
