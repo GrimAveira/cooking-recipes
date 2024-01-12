c89ad56fd898e3c769ddf5f506fe556c4a29db7f
GrimAveira <st.tryed@gmail.com>
2024-01-12 13:00:35 +0400

    Relocating burger button Due to the overlap of the header component with the menu component button was unavailable Changed:     - button has relocated to the shell component

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,25 +1,12 @@
 import Button from "../button/Button";
 import styles from "./Header.module.scss";
 import { logo } from "../../assets";
-import Burger from "../burger/Burger.tsx";
 
-interface IProps {
-  show: boolean;
-  setShow: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-const Header = ({ show, setShow }: IProps) => {
+const Header = () => {
   return (
     <header className={styles.container}>
-      <Burger
-        show={show}
-        className={styles.menu}
-        onClick={() => {
-          setShow((show: boolean) => !show);
-        }}
-      />
       <div className={styles.blur}></div>
-      <a href="/">
+      <a href="/" title="На главную">
         <img className={styles.logo} src={logo} />
       </a>
       <Button title="Вход" className={styles.button} />
--- frontend/src/components/shell/Shell.tsx
+++ frontend/src/components/shell/Shell.tsx
@@ -2,13 +2,22 @@ import Header from "../header/Header";
 import Menu from "../menu/Menu";
 import { useState } from "react";
 import styles from "./Shell.module.scss";
+import Burger from "../burger/Burger";
 
 function Shell() {
   const [show, setShow] = useState<boolean>(false);
 
   return (
     <div className={styles.container}>
-      <Header show={show} setShow={setShow} />
+      <Burger
+        title="Меню"
+        show={show}
+        className={styles.menuButton}
+        onClick={() => {
+          setShow((show: boolean) => !show);
+        }}
+      />
+      <Header />
       <Menu show={show} />
     </div>
   );
