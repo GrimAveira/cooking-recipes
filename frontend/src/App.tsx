9a3cfa9c5774b560759b18c8d51ee7de3c9ad767
GrimAveira <st.tryed@gmail.com>
2024-01-13 16:15:04 +0400

    React router dom and home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CategoryCards from "./components/category-cards/CategoryCards";
 import Footer from "./components/footer/Footer";
 import Shell from "./components/shell/Shell";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CategoryCards />,
+  },
+]);
 
 function App() {
   return (
     <>
       <Shell />
-      <CategoryCards />
+      <RouterProvider router={router} />
       <Footer />
     </>
   );
