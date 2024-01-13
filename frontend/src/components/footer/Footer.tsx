38b1adde1c93db9724cafd4b45d4af016ffb9213
GrimAveira <st.tryed@gmail.com>
2024-01-13 16:13:23 +0400

    Implementing footer Added:     - footer icons and button about Changed:     - footer design and time animation

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,7 +1,42 @@
 import styles from "./Footer.module.scss";
+import { icons } from "../../constants/index";
+
+interface URLIconI {
+  title: string;
+  url: string;
+  svg: string;
+}
+
+const URLIcon = (icon: URLIconI) => {
+  return (
+    <li>
+      <a href={icon.url} title={icon.url} className={styles.iconContainer}>
+        <img className={styles.icon} src={icon.svg} />
+      </a>
+    </li>
+  );
+};
 
 const Footer = () => {
-  return <footer className={styles.container}>Footer</footer>;
+  return (
+    <footer className={styles.container}>
+      <section className={styles.info}>
+        <button className={styles.feedbackButton}>Обратная связь</button>
+      </section>
+      <ul className={styles.about}>
+        {icons.map((icon) => {
+          return (
+            <URLIcon
+              svg={icon.svg}
+              title={icon.title}
+              url={icon.url}
+              key={icon.title}
+            />
+          );
+        })}
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -4,6 +4,8 @@ import broths from "./broths.png";
 import soups from "./soups.png";
 import drinks from "./drinks.png";
 import snacks from "./snacks.png";
+import vk from "./vk_icon.svg";
+import github from "./github_icon.svg";
 
 export const categoryCards = [
   { img: bakes, url: "bakes", title: "Выпечка" },
@@ -18,4 +20,9 @@ export const titles = [
   { title: "ИЗБРАННОЕ", url: "favourites" },
   { title: "ЛИЧНЫЙ КАБИНЕТ", url: "lk" },
   { title: "О САЙТЕ", url: "about" },
+];
+
+export const icons = [
+  { title: "Вконтакте", url: "https://vk.com/id187389901", svg: vk },
+  { title: "GitHub", url: "https://github.com/GrimAveira", svg: github },
 ];
