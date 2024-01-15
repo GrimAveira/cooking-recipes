e1f377f6166c1ba6a11652c01c1e06627e74e9a6
GrimAveira <st.tryed@gmail.com>
2024-01-15 22:03:13 +0400

    Implementation Login component Added:     - InputLabel components

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,18 +1,26 @@
-import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
+import InputLabel from "../../components/labelInput/InputLabel";
 import styles from "./Login.module.scss";
 
 const Login = () => {
-  return (
-    <div className={styles.container}>
-      <p className={styles.p}>Вход</p>
-      <form className={styles.form}>
-        <label htmlFor=""></label>
-        <Input className={styles.input} />
-        <label htmlFor=""></label>
-        <Input className={styles.input} />
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<p className={styles.p}>Вход</p>
+			<form className={styles.form}>
+				<InputLabel
+					className={styles.inputForm}
+					label={"Логин"}
+					input={{ className: styles.input, name: "login" }}
+				/>
+				<InputLabel
+					className={styles.inputForm}
+					label={"Пароль"}
+					input={{ className: styles.input, name: "password", type: "password" }}
+				/>
+				<Button title="Войти" className={styles.enterButton} />
+			</form>
+		</div>
+	);
 };
 
 export default Login;
