4e895964e6ddf5b3d0a12ecd826a460c5fb24d95
GrimAveira <st.tryed@gmail.com>
2024-01-15 22:02:38 +0400

    Added general a css style

--- frontend/src/components/shell/Shell.tsx
+++ frontend/src/components/shell/Shell.tsx
@@ -5,24 +5,32 @@ import styles from "./Shell.module.scss";
 import Burger from "../burger/Burger";
 
 function Shell() {
-  const [show, setShow] = useState<boolean>(false);
+	const [show, setShow] =
+		useState<boolean>(false);
 
-  return (
-    <div className={styles.container}>
-      <Burger
-        show={show}
-        div={{
-          title: "Меню",
-          onClick: () => {
-            setShow((show: boolean) => !show);
-          },
-          className: styles.menuButton,
-        }}
-      />
-      <Header />
-      <Menu show={show} />
-    </div>
-  );
+	return (
+		<div
+			className={
+				styles.container
+			}
+		>
+			<Burger
+				show={show}
+				title={"Меню"}
+				onClick={() => {
+					setShow(
+						(show: boolean) =>
+							!show,
+					);
+				}}
+				className={
+					styles.menuButton
+				}
+			/>
+			<Header />
+			<Menu show={show} />
+		</div>
+	);
 }
 
 export default Shell;
