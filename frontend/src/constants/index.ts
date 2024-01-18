44503cc50362f6928ce1775fdc1e5e740b7e57ea
grimaveira <st.tryed@gmail.com>
2024-01-18 18:40:21 +0400

    Png's for types recipes tabs and Map for translate Added:     - png's     - Map for props driling

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -6,23 +6,43 @@ import drinks from "./drinks.png";
 import snacks from "./snacks.png";
 import vk from "./vk_icon.svg";
 import github from "./github_icon.svg";
+import bakesMinor from "./bakesMinor.png";
+import dessertMinor from "./dessertMinor.png";
+import brothsMinor from "./brothsMinor.png";
+import soupMinor from "./soupMinor.png";
+import drinkMinor from "./drinkMinor.png";
+import snackMinor from "./snackMinor.png";
 
 export const categoryCards = [
-  { img: bakes, url: "bakes", title: "Выпечка" },
-  { img: desserts, url: "desserts", title: "Дессерты" },
-  { img: broths, url: "broths", title: "Бульоны" },
-  { img: soups, url: "soups", title: "Супы" },
-  { img: drinks, url: "drinks", title: "Напитки" },
-  { img: snacks, url: "snacks", title: "Закуски" },
+	{ img: bakes, url: "recipes/bakes", title: "Выпечка" },
+	{ img: desserts, url: "recipes/desserts", title: "Дессерты" },
+	{ img: broths, url: "recipes/broths", title: "Бульоны" },
+	{ img: soups, url: "recipes/soups", title: "Супы" },
+	{ img: drinks, url: "recipes/drinks", title: "Напитки" },
+	{ img: snacks, url: "recipes/snacks", title: "Закуски" },
 ];
 
 export const titles = [
-  { title: "ИЗБРАННОЕ", url: "favourites" },
-  { title: "ЛИЧНЫЙ КАБИНЕТ", url: "lk" },
-  { title: "О САЙТЕ", url: "about" },
+	{ title: "ИЗБРАННОЕ", url: "favourites" },
+	{ title: "ЛИЧНЫЙ КАБИНЕТ", url: "lk" },
+	{ title: "О САЙТЕ", url: "about" },
 ];
 
 export const icons = [
-  { title: "Вконтакте", url: "https://vk.com/id187389901", svg: vk },
-  { title: "GitHub", url: "https://github.com/GrimAveira", svg: github },
+	{ title: "Вконтакте", url: "https://vk.com/id187389901", svg: vk },
+	{ title: "GitHub", url: "https://github.com/GrimAveira", svg: github },
 ];
+
+export interface translateI {
+	background: string;
+	title: string;
+}
+
+export const translates = new Map<string, translateI>([
+	["bakes", { background: bakesMinor, title: "Выпечка" }],
+	["desserts", { background: dessertMinor, title: "Дессерты" }],
+	["broths", { background: brothsMinor, title: "Бульоны" }],
+	["soups", { background: soupMinor, title: "Супы" }],
+	["drinks", { background: drinkMinor, title: "Напитки" }],
+	["snacks", { background: snackMinor, title: "Закуски" }],
+]);
