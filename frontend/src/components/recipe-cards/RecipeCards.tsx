7267d12c74d38dc821c006e0704c62da36252186
grimaveira <st.tryed@gmail.com>
2024-02-15 20:10:06 +0400

    Recipe cards components without functionality Added:     - RecipeCard.tsx with dish recipe     - RecipeCards.tsx container for RecipeCard     - RecipeOptions.tsx options for the dish filter     - Icons for the button's options

---
+++ frontend/src/components/recipe-cards/RecipeCards.tsx
@@ -0,0 +1,11 @@
+import RecipeCard from "../recipe-card/RecipeCard";
+
+const RecipeCards = () => {
+	return (
+		<div>
+			<RecipeCard />
+		</div>
+	);
+};
+
+export default RecipeCards;
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -14,6 +14,10 @@ import drinkMinor from "./drinkMinor.png";
 import snackMinor from "./snackMinor.png";
 import cupcakeType from "./cupcakeType.svg";
 import biscuitType from "./biscuitType.svg";
+import recently from "./recently.svg";
+import favorite from "./favorite.svg";
+
+export const recipeOptions = { recently, favorite };
 
 export const categoryCards = [
 	{ img: bakes, url: "recipes/bakes", title: "Выпечка" },
