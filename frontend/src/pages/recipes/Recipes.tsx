c8a77b9797a378dfd1cc9f107b2cd5160dc9fd8a
grimaveira <st.tryed@gmail.com>
2024-02-15 20:15:21 +0400

    Blanks for the recipe filter Changed:     - new state and filterByOption function in Recipe.tsx

--- frontend/src/pages/recipes/Recipes.tsx
+++ frontend/src/pages/recipes/Recipes.tsx
@@ -3,14 +3,32 @@ import styles from "./Recipes.module.scss";
 import ShellWrapper from "../../hoc/ShellWrapper";
 import MinorHeader from "../../components/minorHeader/MinorHeader";
 import TypesBar from "../../components/typesBar/TypesBar";
+import RecipeCards from "../../components/recipe-cards/RecipeCards";
+import RecipeOptions from "../../components/recipe-options/RecipeOptions";
+import { useEffect, useState } from "react";
 
 const RecipesBase = () => {
 	const { type, subtype } = useParams();
+	const [filter, setFilter] = useState("popular");
+
+	type options = "popular" | "recently";
+
+	function sortByOption(array: Array<string>, option: options) {
+		return array.filter((elem) => elem === option);
+	}
+
+	useEffect(() => {}, [filter]);
 
 	return (
 		<main>
 			<MinorHeader type={subtype ? subtype : type} />
-			{typeof subtype === "undefined" && <TypesBar className={styles.typesBar} type={type} />}
+			<article className={styles.container}>
+				{typeof subtype === "undefined" && <TypesBar className={styles.typesBar} type={type} />}
+				<p className={styles.recipeCount}>{`Всего найдено рецептов: ${223}`}</p>
+				<RecipeOptions setFilter={setFilter} />
+				<div className={styles.divLine} />
+				<RecipeCards />
+			</article>
 		</main>
 	);
 };
