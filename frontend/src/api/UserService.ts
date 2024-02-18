9d7dc55aed00f35bf39af33790db647e5183223e
grimaveira <st.tryed@gmail.com>
2024-02-18 20:13:00 +0400

    Frontend logout/login in UserService.ts

--- frontend/src/api/UserService.ts
+++ frontend/src/api/UserService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { IUserData } from "../pages/registratrion/Registration";
-
+import { IUserData } from "../interfaces";
+axios.defaults.withCredentials = true;
 export default class UserService {
 	static async registration(userData: IUserData) {
 		try {
@@ -14,4 +14,40 @@ export default class UserService {
 			}
 		}
 	}
+	static async authorization(userData: IUserData) {
+		try {
+			const response = await axios.post("http://localhost:3000/api/auth/login", userData);
+			return response.data;
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				throw error.response?.data;
+			} else if (error instanceof Error) {
+				throw error.message;
+			}
+		}
+	}
+	static async logout() {
+		try {
+			const response = await axios.delete("http://localhost:3000/api/auth/logout");
+			return response.data;
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				throw error.response?.data;
+			} else if (error instanceof Error) {
+				throw error.message;
+			}
+		}
+	}
+	// static async isAuth() {
+	// 	try {
+	// 		const response = await axios.delete("http://localhost:3000/api/auth/logout");
+	// 		return response.data;
+	// 	} catch (error) {
+	// 		if (axios.isAxiosError(error)) {
+	// 			throw error.response?.data;
+	// 		} else if (error instanceof Error) {
+	// 			throw error.message;
+	// 		}
+	// 	}
+	// }
 }
