a1560a67daf59c9f79fb73e8e8b70bd7b67e1e70
grimaveira <st.tryed@gmail.com>
2024-02-20 21:12:49 +0400

    Part of tags of Header formed to Auth component

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,57 +1,17 @@
 import styles from "./Header.module.scss";
 import { logo } from "../../assets";
-import CustomLink from "../customLink/CustomLink";
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import Button from "../button/Button";
-import { useMutation } from "react-query";
-import UserService from "../../api/UserService";
-import { promiseSuccess, promiseFail } from "../../functions/toastTrigger";
-import { useNavigate } from "react-router-dom";
-
-const logout = async () => {
-	try {
-		return await UserService.logout();
-	} catch (error) {
-		return error;
-	}
-};
+import Auth from "./Auth";
 
 const Header = () => {
-	const navigate = useNavigate();
-
-	const { isAuth, setIsAuth, setLogin, setRole } = useContext(AuthContext);
-
-	const clickHandler = () => {
-		mutationUser.mutate();
-	};
-
-	const mutationUser = useMutation({
-		mutationFn: logout,
-		onSuccess(message: string) {
-			promiseSuccess(message);
-			setIsAuth(false);
-			setLogin(null);
-			setRole(null);
-			navigate("/");
-		},
-		onError(message: string) {
-			promiseFail(message);
-		},
-	});
-
 	return (
 		<header className={styles.container}>
 			<div className={styles.blur}></div>
+
 			{/*  */}
 			<a href="/" title="На главную">
 				<img className={styles.logo} src={logo} />
 			</a>
-			{isAuth ? (
-				<Button className={styles.button} title={"Выход"} onClick={clickHandler} />
-			) : (
-				<CustomLink to={"/login"} title="Вход" className={styles.button} children={"Вход"} />
-			)}
+			<Auth className={styles.auth} />
 		</header>
 	);
 };
