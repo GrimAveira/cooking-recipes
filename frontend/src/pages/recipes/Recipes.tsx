a95dc763e91398e361abfd3edd0abbcf299204a9
grimaveira <st.tryed@gmail.com>
2024-02-20 21:31:19 +0400

    Part of transefing fetch Recipe logic in new comp

--- frontend/src/pages/recipes/Recipes.tsx
+++ frontend/src/pages/recipes/Recipes.tsx
@@ -1,33 +1,19 @@
 import { useParams } from "react-router-dom";
 import styles from "./Recipes.module.scss";
 import ShellWrapper from "../../hoc/ShellWrapper";
-import MinorHeader from "../../components/minorHeader/MinorHeader";
-import TypesBar from "../../components/typesBar/TypesBar";
-import RecipeCards from "../../components/recipe-cards/RecipeCards";
-import RecipeOptions from "../../components/recipe-options/RecipeOptions";
-import { useEffect, useState } from "react";
+import MinorHeader from "../../components/minor-header/MinorHeader";
+import TypesBar from "../../components/types-bar/TypesBar";
+import RecipeWrapper from "../../components/recipe-cards/RecipeCards";
 
 const RecipesBase = () => {
 	const { type, subtype } = useParams();
-	const [filter, setFilter] = useState("popular");
-
-	type options = "popular" | "recently";
-
-	function sortByOption(array: Array<string>, option: options) {
-		return array.filter((elem) => elem === option);
-	}
-
-	useEffect(() => {}, [filter]);
 
 	return (
 		<main>
 			<MinorHeader type={subtype ? subtype : type} />
 			<article className={styles.container}>
 				{typeof subtype === "undefined" && <TypesBar className={styles.typesBar} type={type} />}
-				<p className={styles.recipeCount}>{`Всего найдено рецептов: ${223}`}</p>
-				<RecipeOptions setFilter={setFilter} />
-				<div className={styles.divLine} />
-				<RecipeCards />
+				<RecipeWrapper type={type} subtype={subtype} />
 			</article>
 		</main>
 	);
