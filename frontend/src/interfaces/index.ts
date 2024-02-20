fda439362289efed9712a234ddd117b29291a6a0
grimaveira <st.tryed@gmail.com>
2024-02-20 20:56:15 +0400

    Recipe service for frontend with interface Added:     - interface IRecipe     - function getAll for fetch recipe

--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -18,3 +18,23 @@ export interface IAuthInfo {
 	login: string;
 	message: string;
 }
+export interface IRecipe {
+	id: number;
+	title: string;
+	description: string;
+	creation_date: string;
+	total_cooking_time: string;
+	active_cooking_time: string;
+	complexity: string;
+	storage_time: string;
+	sevings_number: number;
+	calories_number: number;
+	score_number: number;
+	rating: number;
+	image_path: string;
+	first_name: string;
+	second_name: string;
+	kitchen: string;
+	type: string;
+	subtype: string;
+}
