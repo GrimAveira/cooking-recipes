88772cbafc8f00ff07247ac4da352a29f80c6e72
grimaveira <st.tryed@gmail.com>
2024-02-29 13:50:33 +0400

    The beginning CreateRecipe component

--- frontend/src/pages/create/CreateRoute.tsx
+++ frontend/src/pages/create/CreateRoute.tsx
@@ -1,8 +1,77 @@
+import { useState } from "react";
+import BasicSelect from "../../components/basic-select/BasicSelect";
 import ShellWrapper from "../../hoc/ShellWrapper";
+import { SelectChangeEvent } from "@mui/material/Select";
+import styles from "./CreateRoute.module.scss";
+
+interface IRecipe {
+	title: string;
+	description: string;
+	total_cooking_time: string;
+	active_cooking_time: string;
+	complexity: string;
+	storage_time: string;
+	servings_number: string;
+	calories_number: string;
+	score_number: string;
+	image: string;
+	kitchen: string;
+	type: string;
+	subtype: string;
+}
 
 const CreateRouteBase = () => {
+	const [complexity, setComplexity] = useState<IRecipe>({
+		active_cooking_time: "",
+		calories_number: "",
+		complexity: "",
+		description: "",
+		image: "",
+		kitchen: "",
+		score_number: "",
+		servings_number: "",
+		storage_time: "",
+		subtype: "",
+		title: "",
+		total_cooking_time: "",
+		type: "",
+	});
+
+	// const queries = [
+	// 	{ queryKey: 'todos', queryFn: fetchTodos },
+	// 	{ queryKey: 'users', queryFn: fetchUsers },
+	//   ];
+
+	const changeHandler = (event: SelectChangeEvent) => {
+		event.target;
+		setComplexity(event.target.value as string);
+	};
+
+	const selects = [
+		{
+			onChange: changeHandler,
+			value: complexity,
+			name: "complexity",
+			label: "Сложность готовки",
+			options: [
+				{ value: "1", title: "1" },
+				{ value: "1", title: "1" },
+			],
+		},
+	];
+
 	return (
-		<div>
+		<div className={styles.container}>
+			{selects.map((select) => (
+				<BasicSelect
+					onChange={select.onChange}
+					value={select.value}
+					options={select.options}
+					name={select.name}
+					label={select.label}
+				/>
+			))}
+			АЛО
 			<text />
 		</div>
 	);
