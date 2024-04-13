6e388ffcd1983fe6e510011f495c2235d3c8b2ab
grimaveira <st.tryed@gmail.com>
2024-04-13 17:30:16 +0400

    Pagination and minor fixes

--- frontend/src/components/recipe-cards/RecipeCards.tsx
+++ frontend/src/components/recipe-cards/RecipeCards.tsx
@@ -3,12 +3,21 @@ import RecipeOptions from "../recipe-options/RecipeOptions";
 import styles from "./RecipeCards.module.scss";
 import RecipeCard from "../recipe-card/RecipeCard";
 import { IRecipeFetch } from "../../interfaces";
+import { Pagination } from "@mui/material";
 
 function sortByOption(array: IRecipeFetch[], sortOption: options) {
 	if (sortOption === "popular") array.sort((a, b) => Number(b.rating) - Number(a.rating));
 	else if (sortOption === "recently")
 		array.sort((a, b) => new Date(a.creation_date).getTime() - new Date(b.creation_date).getTime());
-	return array;
+	const pagArray: IRecipeFetch[][] = [];
+	let arrayIndex = -1;
+	array.forEach((recipe, index) => {
+		if (index % 12 == 0) {
+			pagArray.push([recipe]);
+			arrayIndex++;
+		} else pagArray[arrayIndex].push(recipe);
+	});
+	return pagArray;
 }
 
 export type options = "popular" | "recently";
@@ -17,22 +26,41 @@ const RecipeCards = (props: { type?: string; subtype?: string; recipes: IRecipeF
 	const { recipes } = props;
 
 	const [sortOption, setSortOption] = useState<options>("popular");
-	const [sortedRecipes, setSortedRecipes] = useState<IRecipeFetch[]>([]);
+	const [sortedRecipes, setSortedRecipes] = useState<IRecipeFetch[][]>([]);
+	const [page, setPage] = useState<number>(1);
 
 	useEffect(() => {
 		setSortedRecipes(sortByOption(recipes, sortOption));
 	}, [recipes, sortOption]);
 
+	const paginationHandler = (event: React.ChangeEvent<unknown>, value: number) => {
+		event.preventDefault();
+		setPage(value);
+	};
+
 	return (
 		<>
 			<p className={styles.recipeCount}>{`Всего найдено рецептов: ${recipes?.length}`}</p>
-			<RecipeOptions className={styles.options} setFilter={setSortOption} />
+			<RecipeOptions className={styles.options} setFilter={setSortOption} sortOption={sortOption} />
 			<div className={styles.divLine} />
 			<div className={styles.containerCards}>
-				{sortedRecipes?.map((recipe) => {
+				{sortedRecipes[page - 1]?.map((recipe) => {
 					return <RecipeCard key={recipe.id} {...recipe} />;
 				})}
 			</div>
+			<Pagination
+				className={styles.pagination}
+				count={Math.floor(recipes.length / 12) + 1}
+				sx={{
+					"& .MuiPaginationItem-root": {
+						color: "#fff",
+					},
+				}}
+				page={page}
+				onChange={paginationHandler}
+				size="large"
+				color="primary"
+			/>
 		</>
 	);
 };
